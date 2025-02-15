5de61ee2d6ba41bcf016bf5306e538012db01391
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2025-02-15 00:44:28 -0500

    Code deduplication for haproxy, and modularizes haproxy into modules/

--- systems/relay-node/modules/haproxy/default.nix
+++ systems/relay-node/modules/haproxy/default.nix
@@ -1,9 +1,10 @@
 {
-  config,
-  lib,
-  pkgs,
   ...
 }: {
+  imports = [
+    ../haproxy_base
+  ];
+
   networking.firewall.allowedTCPPorts = [
     1337 # Sybil
     3336 # Terry
@@ -12,25 +13,8 @@
     # 4447 # event-us
     # 4337 # event-eu
   ];
-  systemd.tmpfiles.rules = [
-    "d /var/lib/haproxy 770 ${config.services.haproxy.user} ${config.services.haproxy.group}"
-  ];
-  services.haproxy = {
-    enable = true;
-    config =
-      "# ==== GLOBAL CONFIG ====\n" +
-      builtins.readFile ../../../haproxy_relay_global.conf +
-      "\n\n# ==== LOCAL CONFIG ====\n" +
-      builtins.readFile ./haproxy.conf;
-  };
-  systemd.services.haproxy = {
-    serviceConfig = {
-      AmbientCapabilities = lib.mkForce "CAP_NET_BIND_SERVCE CAP_NET_RAW";
-      CapabilityBoundingSet = "CAP_NET_BIND_SERVICE CAP_NET_RAW";
-    };
-    environment = {
-      PROMETHEUS_PORT = "8405";
-    };
-  };
-  services.tailscale.useRoutingFeatures = "server"; # IP Forwarding
+
+  services.haproxy.config =
+    "\n\n# ==== LOCAL CONFIG ====\n" +
+    builtins.readFile ./haproxy.conf;
 }
