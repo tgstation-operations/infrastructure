baec37f684fa57ed6328591e0391eb6af5f1af11
Riggle <27156122+RigglePrime@users.noreply.github.com>
2025-02-16 16:05:52 +0100

    Update modules/tgs/default.nix
    Co-authored-by: Jordan Dominion <Cyberboss@users.noreply.github.com>

--- modules/tgs/default.nix
+++ modules/tgs/default.nix
@@ -82,7 +82,7 @@ in {
     );
   };
   systemd.services.tgstation-server = {
-    After = ["mysql.service"];
+    wants = ["mysql.service"];
   };
   age.secrets.rsc-cdn = {
     file = ./secrets/rsc-cdn.age;
