2c3413a9f226db63951e7343478885a5ad094ce2
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2025-02-24 10:53:16 +0100

    Haproxy cleanup

--- systems/edge-nodes/modules/haproxy_base/default.nix
+++ systems/edge-nodes/modules/haproxy_base/default.nix
@@ -1,26 +1,11 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  systemd.tmpfiles.rules = [
-    "d /var/lib/haproxy 770 ${config.services.haproxy.user} ${config.services.haproxy.group}"
+{...}: {
+  imports = [
+    ../../../../modules/haproxy_common.nix
   ];
   services.haproxy = {
-    enable = true;
     config =
       "# ==== GLOBAL CONFIG ====\n"
       + builtins.readFile ./haproxy.conf;
-  };
-  systemd.services.haproxy = {
-    serviceConfig = {
-      AmbientCapabilities = lib.mkForce "CAP_NET_BIND_SERVCE CAP_NET_RAW";
-      CapabilityBoundingSet = "CAP_NET_BIND_SERVICE CAP_NET_RAW";
-    };
-    environment = {
-      PROMETHEUS_PORT = "8405";
-    };
   };
   services.tailscale.useRoutingFeatures = "server"; # IP Forwarding
 }
