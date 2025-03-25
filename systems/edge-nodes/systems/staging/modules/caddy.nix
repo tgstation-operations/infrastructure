020e226aa2e946097a4547884ea67cea504b98ae
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2025-03-25 09:43:20 +0100

    Nyoops

--- systems/edge-nodes/systems/staging/modules/caddy.nix
+++ systems/edge-nodes/systems/staging/modules/caddy.nix
@@ -70,8 +70,7 @@
       plugins = [
         "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb" # Module to retrieve trusted proxy IPs from cloudflare
       ];
-      hash = "sha256-giy2YToFmJuDxX26OF8psAmkVh4R4uFFNHXWZ2dVLVA=
-";
+      hash = "sha256-giy2YToFmJuDxX26OF8psAmkVh4R4uFFNHXWZ2dVLVA=";
     };
     enableReload = true; # Reload caddy instead of restarting it on config changes
     globalConfig = ''
