a6653616f61802327d5a68369d7f33f11ef59f5d
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-04-20 14:46:06 -0400

    Fix export paths

--- systems/game-servers/modules/tgs/default.nix
+++ systems/game-servers/modules/tgs/default.nix
@@ -11,65 +11,64 @@
   ];
 
   # `<instance>/Configuration/EventScripts` is symlinked to these directories
-  environment.etc."tgs-EventScripts.d" = {
-    tg = {
-      "DreamDaemonPreLaunch.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/DreamDaemonPreLaunch.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "parse-server.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/parse-server.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "PostCompile.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/PostCompile.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "PreCompile.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/PreCompile.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "tg-Roundend.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/tg-Roundend.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "update-config.sh" = {
-        text = (builtins.readFile ./EventScripts/tg/update-config.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
+  environment.etc."tgs-EventScripts.d/tg" = {
+    #TG
+    "tgs-EventScripts.d/tg/DreamDaemonPreLaunch.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/DreamDaemonPreLaunch.sh);
+      group = "tgstation-server";
+      mode = "0755";
     };
-    tgmc = {
-      "DreamDaemonPreLaunch.sh" = {
-        text = (builtins.readFile ./EventScripts/tgmc/DreamDaemonPreLaunch.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "PostCompile.sh" = {
-        text = (builtins.readFile ./EventScripts/tgmc/PostCompile.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "PreCompile.sh" = {
-        text = (builtins.readFile ./EventScripts/tgmc/PreCompile.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "tg-Roundend.sh" = {
-        text = (builtins.readFile ./EventScripts/tgmc/tg-Roundend.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
-      "update-config.sh" = {
-        text = (builtins.readFile ./EventScripts/tgmc/update-config.sh);
-        group = "tgstation-server";
-        mode = "0755";
-      };
+    "tgs-EventScripts.d/tg/parse-server.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/parse-server.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tg/PostCompile.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/PostCompile.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tg/PreCompile.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/PreCompile.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tg/tg-Roundend.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/tg-Roundend.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tg/update-config.sh" = {
+      text = (builtins.readFile ./EventScripts/tg/update-config.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+
+    #TGMC
+    "tgs-EventScripts.d/tgmc/DreamDaemonPreLaunch.sh" = {
+      text = (builtins.readFile ./EventScripts/tgmc/DreamDaemonPreLaunch.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tgmc/PostCompile.sh" = {
+      text = (builtins.readFile ./EventScripts/tgmc/PostCompile.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tgmc/PreCompile.sh" = {
+      text = (builtins.readFile ./EventScripts/tgmc/PreCompile.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tgmc/tg-Roundend.sh" = {
+      text = (builtins.readFile ./EventScripts/tgmc/tg-Roundend.sh);
+      group = "tgstation-server";
+      mode = "0755";
+    };
+    "tgs-EventScripts.d/tgmc/update-config.sh" = {
+      text = (builtins.readFile ./EventScripts/tgmc/update-config.sh);
+      group = "tgstation-server";
+      mode = "0755";
     };
   };
 
