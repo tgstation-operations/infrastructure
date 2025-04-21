faa3f9cddad7c7d1541e5c61582bddf2622483bb
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-04-21 10:35:51 -0400

    make this look nicer and make dreamlua compiling work

--- systems/game-servers/modules/tgs/EventScripts/tg/PreCompile.sh
+++ systems/game-servers/modules/tgs/EventScripts/tg/PreCompile.sh
@@ -3,54 +3,56 @@
 set -e
 set -x
 
-#load dep exports
-#need to switch to game dir for Dockerfile weirdness
-original_dir="${TGS_INSTANCE_ROOT}/Configuration/EventScriptsScratch"
+# move to deployment folder
 cd "$1"
+
+# load dependencies
 . dependencies.sh
-mkdir -p "$original_dir"
-cd "$original_dirh"
 
-# update rust-g
+# create a dedicated folder for running the script
+work_directory="${TGS_INSTANCE_ROOT}/Configuration/EventScriptsScratch"
+mkdir -p $work_directory
+
+cd $work_directory
+
+echo "rust-g: deployment begin"
 if [ ! -d "rust-g" ]; then
-        echo "Cloning rust-g..."
-        git clone https://github.com/tgstation/rust-g
-        cd rust-g
+  echo "rust-g: cloning"
+  git clone https://github.com/tgstation/rust-g >/dev/null
+  cd rust-g
 else
-        echo "Fetching rust-g..."
-        cd rust-g
-        git fetch
+  echo "rust-g: fetching"
+  cd rust-g
+  git fetch >/dev/null
 fi
-
-echo "Deploying rust-g..."
-git checkout "$RUST_G_VERSION"
-
+echo "rust-g: checkout"
+git checkout "$RUST_G_VERSION" >/dev/null
+echo "rust-g: building"
 cargo build --ignore-rust-version --release --target=i686-unknown-linux-gnu --features all
 mv target/i686-unknown-linux-gnu/release/librust_g.so "$1/librust_g.so"
+cd "$work_directory"
+echo "rust-g: deployment finish"
 
-#cd "$original_dir"
-
-# update dreamluau
-#if [ ! -d "dreamluau" ]; then
-        #echo "Cloning dreamluau..."
-        #git clone https://github.com/tgstation/dreamluau
-        #cd dreamluau
-#else
-        #echo "Fetching dreamlaua..."
-        #cd dreamluau
-        #git fetch
-#fi
-
-echo "Deploying Dreamlaua..."
-#git checkout "$DREAMLUAU_VERSION"
-
-# cargo build --ignore-rust-version --release --target=i686-unknown-linux-gnu
-# mv target/i686-unknown-linux-gnu/release/libdreamluau.so "$1/libdreamluau.so"
-
-# Temporary workaround. Expecting prebuild libdreamluau.so in Configuration directory
-cp ../../libdreamluau.so "$1/libdreamluau.so"
+echo "dreamluau: deployment begin"
+if [ ! -d "dreamluau" ]; then
+  echo "dreamluau: cloning"
+  git clone https://github.com/tgstation/dreamluau >/dev/null
+  cd dreamluau
+else
+  echo "dreamluau: fetching"
+  cd dreamluau
+  git fetch >/dev/null
+fi
+echo "dreamluau: checkout"
+git checkout "$DREAMLUAU_VERSION" >/dev/null
+echo "dreamluau: building"
+env LIBCLANG_PATH="$(find /nix/store/*-clang-*-lib)/lib" cargo build --ignore-rust-version --release --target=i686-unknown-linux-gnu
+mv target/i686-unknown-linux-gnu/release/libdreamluau.so "$1/libdreamluau.so"
+cd "$work_directory"
+echo "dreamluau: deployment finish"
 
 # compile tgui
-echo "Compiling tgui..."
+echo "tgui: deployment begin"
 cd "$1"
-env TG_BOOTSTRAP_CACHE="$original_dir" TG_BOOTSTRAP_NODE_LINUX=1 CBT_BUILD_MODE="TGS" tools/bootstrap/node tools/build/build.js
+env TG_BOOTSTRAP_CACHE="$work_directory" TG_BOOTSTRAP_NODE_LINUX=1 CBT_BUILD_MODE="TGS" tools/bootstrap/node tools/build/build.js
+echo "tgui: deployment finish"
