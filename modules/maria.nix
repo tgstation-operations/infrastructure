b859e1067db06ea570b40f6ecfc19f53e702c9ab
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-06-06 10:39:13 -0400

    galera conf

--- modules/maria.nix
+++ modules/maria.nix
@@ -14,6 +14,17 @@
         thread_handling = "pool-of-threads"; # TP size is num of cpus, 32 on satan
         max_connections = 302;
       };
+      galera = {
+        wsrep_provider = "/usr/lib/libgalera_smm.so";
+        wsrep_on = "ON";
+        wsrep_cluster_name = "\"/tg/ Cluster\"";
+        wsrep_cluster_address = "gcomm://tgsatan.tg.lan,tg-db-cluster-node-neptune.tg.lan,tg-db-cluster-node-solar.tg.lan,blockmoths.tg.lan";
+        binlog_format = "row";
+        default_storage_engine = "InnoDB";
+        innodb_autoinc_lock_mode = 2;
+        innodb_doublewrite = 1;
+        wsrep_slave_threads = 4;
+      };
     };
   };
 }
