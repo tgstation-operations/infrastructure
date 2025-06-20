cdcb26ce72ba745f73935203e0eab5b08bd75edb
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-06-20 10:21:04 -0400

    they also need to be decryptable by me kekw

--- systems/game-servers/systems/tgsatan/secrets/cockroachdb/secrets.nix
+++ systems/game-servers/systems/tgsatan/secrets/cockroachdb/secrets.nix
@@ -1,10 +1,11 @@
 let
+  users = import ../../../../../../modules/ssh_keys_by_group.nix {group = "db-operator";};
   system = import ../../../../../../modules/ssh_keys_systems.nix;
 in
   with system; {
-    "tgsatan.ca.crt".publicKeys = [tgsatan];
-    "tgsatan.node.crt".publicKeys = [tgsatan];
-    "tgsatan.node.key".publicKeys = [tgsatan];
-    "client.cockroachdb.crt".publicKeys = [tgsatan];
-    "client.cockroachdb.key".publicKeys = [tgsatan];
+    "tgsatan.ca.crt".publicKeys = [tgsatan] ++ users;
+    "tgsatan.node.crt".publicKeys = [tgsatan] ++ users;
+    "tgsatan.node.key".publicKeys = [tgsatan] ++ users;
+    "client.cockroachdb.crt".publicKeys = [tgsatan] ++ users;
+    "client.cockroachdb.key".publicKeys = [tgsatan] ++ users;
   }
