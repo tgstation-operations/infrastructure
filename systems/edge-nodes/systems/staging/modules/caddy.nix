8042f2f872a91c5412cf29617f419668839432e9
scriptis <me@scriptis.net>
2025-07-12 12:46:26 -0500

    fix: more sha256 updates

--- systems/edge-nodes/systems/staging/modules/caddy.nix
+++ systems/edge-nodes/systems/staging/modules/caddy.nix
@@ -69,7 +69,7 @@
       plugins = [
         "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb" # Module to retrieve trusted proxy IPs from cloudflare
       ];
-      hash = "sha256-ntYZso4gaTMdQ3AkX0dk/EpfR924tdaaMdgbXvwX3Yo=";
+      hash = "sha256-w0pJEcwbawr9WKvnyWO++gGHYRUUUxGmGYkXqRvCQ8A=";
     };
     enableReload = true; # Reload caddy instead of restarting it on config changes
     globalConfig = ''
