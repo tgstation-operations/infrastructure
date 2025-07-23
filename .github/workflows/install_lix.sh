7190df23e4a9f852610aa2fe8276aac762a84f92
scriptis <me@scriptis.net>
2025-07-23 14:07:32 -0500

    fix: update install script

--- .github/workflows/install_lix.sh
+++ .github/workflows/install_lix.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --init none
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --init none
+
+mkdir -p /etc/nix
+
+echo "$EXTRA_NIX_CONFIG" >> /etc/nix/nix.conf
