3d4dd274bda10d070defb8106af6a7f25342711a
scriptis <me@scriptis.net>
2025-07-23 14:16:41 -0500

    fix: inline conf

--- .github/workflows/install_lix.sh
+++ .github/workflows/install_lix.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --enable-flakes
-
-mkdir -p /etc/nix
-
-echo "$EXTRA_NIX_CONFIG" | sudo tee -a /etc/nix/nix.conf
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --enable-flakes --extra-conf "$EXTRA_NIX_CONFIG"
