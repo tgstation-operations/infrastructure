4b8a409437cc495056cc3030cc4cf159ec30e074
scriptis <me@scriptis.net>
2025-07-23 14:19:35 -0500

    fix: show config

--- .github/workflows/install_lix.sh
+++ .github/workflows/install_lix.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --enable-flakes --extra-conf "$EXTRA_NIX_CONFIG"
+
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+nix config show
