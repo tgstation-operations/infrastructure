2c275c0685287d1d1021ee54dbc4003a94ec4e8b
scriptis <me@scriptis.net>
2025-07-23 14:15:29 -0500

    fix: try systemd mode

--- .github/workflows/install_lix.sh
+++ .github/workflows/install_lix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --init none
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-confirm --enable-flakes
 
 mkdir -p /etc/nix
 
