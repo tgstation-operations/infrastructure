b09d1b6f71ea8369057b615a5c0cce43d1c869b4
scriptis <me@scriptis.net>
2025-08-10 15:16:16 -0500

    fix: boot sector

--- systems/game-servers/systems/blockmoths/disko.nix
+++ systems/game-servers/systems/blockmoths/disko.nix
@@ -7,6 +7,15 @@
         content = {
           type = "gpt";
           partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
             zfs = {
               size = "100%";
               content = {
