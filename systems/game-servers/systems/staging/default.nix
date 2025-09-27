fe61d9626d7b6fb20e85d3a5f377c756d23e9583
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-09-27 13:24:08 -0400

    Forgor staging

--- systems/game-servers/systems/staging/default.nix
+++ systems/game-servers/systems/staging/default.nix
@@ -9,6 +9,7 @@
   hw = inputs.nixos-hardware.nixosModules;
   baseModules = [
     (import hw.common-cpu-intel)
+    inputs.tg-public-log-parser.nixosModules.default
     inputs.tgstation-server.nixosModules.default
   ];
   localModules = [
