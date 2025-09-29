bc8262a7c168956a715de482e46bfad279fe7d64
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-09-29 16:17:53 -0400

    extra s

--- systems/edge-nodes/systems/us-lemon.nix
+++ systems/edge-nodes/systems/us-lemon.nix
@@ -13,7 +13,7 @@
   system.stateVersion = "24.11";
   nixpkgs.hostPlatform = "x86_64-linux";
   boot = {
-    loaders.grub = {
+    loader.grub = {
       efiSupport = true;
       efiInstallAsRemovable = true;
     };
