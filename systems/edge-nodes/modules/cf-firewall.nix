93e366c627f10ee1af7745ae96b186279f4482a3
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-10-05 22:42:40 -0400

    Actually read the files

--- systems/edge-nodes/modules/cf-firewall.nix
+++ systems/edge-nodes/modules/cf-firewall.nix
@@ -3,14 +3,14 @@
   pkgs,
   ...
 }: let
-  ips-v4 = builtins.fetchurl {
+  ips-v4 = (builtins.readFile builtins.fetchurl {
     url = "https://www.cloudflare.com/ips-v4";
     sha256 = "sha256:0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
-  };
-  ips-v6 = builtins.fetchurl {
+  });
+  ips-v6 = (builtins.readFile builtins.fetchurl {
     url = "https://www.cloudflare.com/ips-v6";
     sha256 = "sha256:1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
-  };
+  });
 in {
   networking.firewall.allowedTCPPorts = [
     # These two are commented out on purpose, a custom firewall is used to only allow access to cloudflare IPs
