5c6842d9eecbffa7f2bc9868e72a8590d8f3101b
distributivgesetz <distributivgesetz93@gmail.com>
2025-10-08 15:14:07 +0200

    move postgres to separate module

--- systems/game-servers/systems/tgsatan/default.nix
+++ systems/game-servers/systems/tgsatan/default.nix
@@ -15,6 +15,7 @@
   localModules = [
     ../../../../modules/fail2ban.nix
     ../../../../modules/maria.nix
+    ../../../../modules/postgres.nix
     ../../../../modules/openssh.nix
     ../../../../modules/tailscale.nix
     ../../../../modules/restic.nix
@@ -121,6 +122,40 @@ in {
         domain = "tgsatan.tg.lan";
       };
     };
+  };
+
+  services.postgresql = {
+    enable = true;
+    # If you change this, you will need to perform manual cleanup
+    # of removed users
+    ensureUsers = [
+      {
+        name = "root";
+      }
+      # {
+      #   name = "tgstation";
+      #   ensureDBOwnership = true;
+      # }
+      # {
+      #   name = "tgmc";
+      #   ensureDBOwnership = true;
+      # }
+      {
+        name = "grafana";
+        ensureDBOwnership = true;
+      }
+      {
+        name = "atticd";
+        ensureDBOwnership = true;
+      }
+    ];
+
+    ensureDatabases = [
+      # "tgstation";
+      # "tgmc";
+      "grafana"
+      "atticd"
+    ];
   };
 
   virtualisation.oci-containers = {
