0bdb68b2dc2f3432258d8fb62cead30f99127678
distributivgesetz <distributivgesetz93@gmail.com>
2025-11-03 21:36:15 +0100

    use heredoc

--- systems/edge-nodes/systems/lemon/modules/mediawiki/default.nix
+++ systems/edge-nodes/systems/lemon/modules/mediawiki/default.nix
@@ -98,7 +98,9 @@ in {
         };
         phases = ["unpackPhase" "patchPhase" "installPhase"];
         patchPhase = ''
-          cat ${./TgForumAuthProvider.php} > src/AuthenticationProvider/TgForumAuthProvider.php
+          cat <<'EOF'
+          ${builtins.readFile ./TgForumAuthProvider.php}
+          EOF > src/AuthenticationProvider/TgForumAuthProvider.php
         '';
         installPhase = ''
           mkdir -p $out
