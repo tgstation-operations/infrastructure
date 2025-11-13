ad53cb94fc03d6d41ca991f1e2ac58e3254ba29a
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-11-12 19:07:37 -0500

    Fix

--- systems/edge-nodes/systems/lemon/modules/bab/default.nix
+++ systems/edge-nodes/systems/lemon/modules/bab/default.nix
@@ -14,8 +14,6 @@
     npmDepsHash = "sha256-dK8gACPM9GIZS5GvDfsssHm8+Y7IPY9AVI6d9gC7Myo=";
     preBuild = ''
       sed -i 's/"name": "bab",/"name": "bab","bin":{"bab":"dist\/index.js"},/g' package.json
-    '';
-    postBuild = ''
       npm run generateDbClient
     '';
   };
