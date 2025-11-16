eb50b3afc08eff7cf0eeec01f71cc7becaacff56
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-11-16 16:51:26 -0500

    Fix age file path

--- systems/game-servers/modules/garage/default.nix
+++ systems/game-servers/modules/garage/default.nix
@@ -15,7 +15,7 @@ in
   networking.firewall.allowedTCPPorts = [
     3901 # RPC
   ];
-  age.secrets.garage.file = ../secrets/garage.age;
+  age.secrets.garage.file = ../../secrets/garage.age;
 
   services.garage = {
     enable = true;
