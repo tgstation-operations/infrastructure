5aaca66eaf9fcb4fb8102f03285f8eaba67271e9
warc <tin.warc@gmail.com>
2026-01-01 01:00:37 -0500

    Update PreCompile.sh
    time and date

--- systems/game-servers/modules/tgs/EventScripts/cool/PreCompile.sh
+++ systems/game-servers/modules/tgs/EventScripts/cool/PreCompile.sh
@@ -55,3 +55,18 @@ cp "${TGS_INSTANCE_ROOT}/Configuration/EventScripts.old/libauxcpu_byondapi.so" "
 
 cd "$work_directory"
 echo "auxcpu: deployment finish"
+
+# Change the build day & month, and all that, so we get nice snazzy holiday/event
+# stuff. (e.g. Halloween, Xmas, etc.)
+
+
+# match    V   this       V  & 1 or more nums - replace with matched bit in parens, plus the relevant day/month/hour/minute
+sed -Ei "s/(BUILD_TIME_DAY)\s+[[:digit:]]+/\1 `date +%-d`/" "${1}/_std/__build.dm"
+sed -Ei "s/(BUILD_TIME_MONTH)\s+[[:digit:]]+/\1 `date +%-m`/" "${1}/_std/__build.dm"
+sed -Ei "s/(BUILD_TIME_HOUR)\s+[[:digit:]]+/\1 `date +%-H`/" "${1}/_std/__build.dm"
+sed -Ei "s/(BUILD_TIME_MINUTE)\s+[[:digit:]]+/\1 `date +%-M`/" "${1}/_std/__build.dm"
+
+RSC_URL = "https\:\/\/rsc.tgstation13.org\/coolstation.rsc.zip"
+sed -Er "s/(\#define.PRELOAD_RSC_URL\s+).+/\1\"${RSC_URL}\"/g" "${1}/_std/__build.dm"
+
+echo "injected time and date to build.dm"
