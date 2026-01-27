33a649241ec3a67d556ce84609a2d84e03d7c90e
Jordan Dominion <dominion@tgstation13.org>
2026-01-27 17:59:51 -0500

    Update systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
    Co-authored-by: Zephyr <12817816+ZephyrTFA@users.noreply.github.com>

--- systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
+++ systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-export GH_TOKEN=$2
-
-@NIX_GH_PATH@/bin/gh -R tgstation/tgstation workflow run compile_changelogs.yml
+set -e
+export GH_TOKEN="$2"
+export GH_REPO="tgstation/tgstation"
+alias gh='@NIX_GH_PATH@/bin/gh'
+gh workflow run compile_changelogs.yml > /dev/null
+gh run watch $(gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
 
 # the ghetto
 @NIX_COREUTILS_PATH@/bin/sleep 45
