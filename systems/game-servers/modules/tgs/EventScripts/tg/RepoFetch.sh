cbabbb82202dd88deeb06c0e2d37943eaba3d665
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-01-27 18:23:25 -0500

    It's the quotes, right?

--- systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
+++ systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
@@ -3,7 +3,7 @@
 set -e
 export GH_TOKEN="$2"
 export GH_REPO="tgstation/tgstation"
-alias gh='@NIX_GH_PATH@/bin/gh'
+alias gh=@NIX_GH_PATH@/bin/gh
 gh workflow run compile_changelogs.yml > /dev/null
 gh run watch $(gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
 
