809d28eb4d703731ac4b2ad5661b37d6cf294630
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-01-27 18:23:45 -0500

    Who left this in?

--- systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
+++ systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
@@ -6,6 +6,3 @@ export GH_REPO="tgstation/tgstation"
 alias gh=@NIX_GH_PATH@/bin/gh
 gh workflow run compile_changelogs.yml > /dev/null
 gh run watch $(gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
-
-# the ghetto
-@NIX_COREUTILS_PATH@/bin/sleep 45
