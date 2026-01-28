2ba3956333c4987501be7b0f56e326d9ddfc7007
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-01-27 19:12:24 -0500

    Aliasing is not working

--- systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
+++ systems/game-servers/modules/tgs/EventScripts/tg/RepoFetch.sh
@@ -3,6 +3,6 @@
 set -e
 export GH_TOKEN="$2"
 export GH_REPO="tgstation/tgstation"
-alias gh=@NIX_GH_PATH@/bin/gh
-gh workflow run compile_changelogs.yml > /dev/null
-gh run watch $(gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
+
+@NIX_GH_PATH@/bin/gh workflow run compile_changelogs.yml > /dev/null
+@NIX_GH_PATH@/bin/gh run watch $(@NIX_GH_PATH@/bin/gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
