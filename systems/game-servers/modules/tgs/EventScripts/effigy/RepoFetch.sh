e0ee3ba88a644d30d19e12c47641f374cd8086af
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-01-30 19:35:57 -0500

    explicit branch name

--- systems/game-servers/modules/tgs/EventScripts/effigy/RepoFetch.sh
+++ systems/game-servers/modules/tgs/EventScripts/effigy/RepoFetch.sh
@@ -4,5 +4,5 @@ set -e
 export GH_TOKEN="$2"
 export GH_REPO="effigy-se/effigy"
 
-@NIX_GH_PATH@/bin/gh workflow run compile_changelogs.yml > /dev/null
+@NIX_GH_PATH@/bin/gh workflow run --ref main compile_changelogs.yml > /dev/null
 @NIX_GH_PATH@/bin/gh run watch $(@NIX_GH_PATH@/bin/gh run list -wcompile_changelogs.yml --json databaseId --limit 1 -q ".[0].databaseId") > /dev/null
